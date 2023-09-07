d5e8fcf95a7c7d051e098c7ab45deb73628b8237
hungryjireh <hungryjireh@gmail.com>
2023-09-07 16:52:24 +0800

    JS101: calculator bonus features

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const calculatorMessages = require('./calculator_messages.json');
 
 // Ask the user for the first number.
 // Ask the user for the second number.
@@ -14,43 +15,67 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to Calculator!');
-
-prompt('What\'s the first number?');
-let number1 = readline.question();
-while (invalidNumber(number1)) {
-  prompt('Hmm... that doesn\'t look like a valid number');
-  number1 = readline.question();
+function getNumber(language) {
+  let number = readline.question();
+  while (invalidNumber(number)) {
+    prompt(calculatorMessages[language].getNumberError);
+    number = readline.question();
+  }
+  return number;
 }
 
-prompt('What\'s the second number?');
-let number2 = readline.question();
-while (invalidNumber(number2)) {
-  prompt('Hmm... that doesn\'t look like a valid number');
-  number2 = readline.question();
+function operationResult(number1, number2, operation) {
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+  return output;
 }
 
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3 or 4');
-  operation = readline.question();
+function calculator(language) {
+  prompt(calculatorMessages[language].getFirstNumber);
+  const number1 = getNumber(language);
+
+  prompt(calculatorMessages[language].getSecondNumber);
+  const number2 = getNumber(language);
+
+  prompt(calculatorMessages[language].getOperation);
+  let operation = readline.question();
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(calculatorMessages.language.getOperationError);
+    operation = readline.question();
+  }
+
+  const output = operationResult(number1, number2, operation);
+  prompt(`${calculatorMessages[language].result} ${output}.`);
 }
 
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
+prompt(calculatorMessages.getLanguage);
+let language = readline.question();
+while (!['1', '2'].includes(language)) {
+  prompt(calculatorMessages.getLanguageError);
+  language = readline.question();
 }
 
-prompt(`The result is ${output}.`);
+prompt(calculatorMessages[language].welcomeMessage);
+let continueCalculator = true;
+while (continueCalculator) {
+  calculator(language);
+
+  prompt(calculatorMessages[language].performAnotherCalculation);
+  let checkContinueCalculator = readline.question();
+  if (checkContinueCalculator && !['yes', 'y'].includes(checkContinueCalculator.toLowerCase())) {
+    continueCalculator = false;
+  }
+}
