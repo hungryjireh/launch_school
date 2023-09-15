f19c2541244a2b9b006e77e165672cbabb884f1c
hungryjireh <hungryjireh@gmail.com>
2023-09-15 08:44:52 +0800

    JS101: Small Problems: Easy 1 Question 2

---
+++ JS101/small_problems/easy_1/2_odd_numbers.js
@@ -0,0 +1,21 @@
+/*
+ — problem: log all ODD numbers between 1 to 99 (inclusive), with each answer
+   on a new line
+— examples: 1, 3, 5, ...
+— input types: 1, 2, 3, ..., 99
+— expected output types: 1, 3, 5, ..., 99
+- rules: 1 and 99 are included
+— algo: for each number, determine if remainder === 1.
+    If so, log the number
+    else, skip
+*/
+
+for (let number = 1; number < 100; number += 2) {
+  console.log(number);
+}
+
+for (let number = 0; number < 100; number += 1) {
+  if (number % 2 === 1) {
+    console.log(number);
+  }
+}
