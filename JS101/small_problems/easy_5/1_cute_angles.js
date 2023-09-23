d6a28b11039a3dec7ab21f78299e119312af12fc
hungryjireh <hungryjireh@gmail.com>
2023-09-23 23:20:25 +0800

    JS101: Small Problems: Easy 5 Question 1

--- JS101/small_problems/easy_5/1_cute_angles.js
+++ JS101/small_problems/easy_5/1_cute_angles.js
@@ -19,6 +19,7 @@ const SECONDS_PER_MINUTE = 60;
 
 function dms(inputNumber) {
   const inputNumberInteger = getInteger(inputNumber);
+  const validDegree = getValidDegree(Number(inputNumberInteger));
   const inputNumberDecimal = getDecimal(inputNumber);
   if (inputNumberDecimal !== 0) {
     const minute = Number(inputNumberDecimal) * MINUTES_PER_DEGREE;
@@ -28,10 +29,10 @@ function dms(inputNumber) {
     if (minuteDecimal !== 0) {
       const degree = Math.round(Number(minuteDecimal) * SECONDS_PER_MINUTE);
       const degreeString = padZeroes(degree);
-      return `${inputNumberInteger}°${minuteIntegerString}'${degreeString}"`;
+      return `${validDegree}°${minuteIntegerString}'${degreeString}"`;
     }
   }
-  return `${inputNumberInteger}°00'00"`;
+  return `${validDegree}°00'00"`;
 }
 
 function getInteger(floatingNumber) {
@@ -43,7 +44,7 @@ function getDecimal(floatingNumber) {
   const inputIntegerStringArray = floatingNumber.toString().split(".");
   if (inputIntegerStringArray.length > 1) {
     const decimalPlace = 1 / Math.pow(10, inputIntegerStringArray[1].length);
-    return inputIntegerStringArray[1] * decimalPlace;
+    return Number(inputIntegerStringArray[1]) * decimalPlace;
   }
   return 0;
 }
@@ -52,9 +53,31 @@ function padZeroes(integer) {
   return (integer.toString().length < 2) ? `0${integer}` : `${integer}`;
 }
 
+function getValidDegree(degree) {
+  if (degree < 0) {
+    let negativeDegree = degree + 360;
+    while (negativeDegree < 0) {
+      negativeDegree += 360;
+    }
+    return negativeDegree;
+  } else if (degree > 360) {
+    let degreeOver360 = degree - 360;
+    while (degreeOver360 > 360) {
+      degreeOver360 -= 360;
+    }
+    return degreeOver360;
+  }
+  return degree;
+}
+
 console.log(dms(30));           // 30°00'00"
 console.log(dms(76.73));        // 76°43'48"
 console.log(dms(254.6));        // 254°35'59"
 console.log(dms(93.034773));    // 93°02'05"
 console.log(dms(0));            // 0°00'00"
 console.log(dms(360));          // 360°00'00" or 0°00'00"
+
+console.log(dms(-1));   // 359°00'00"
+console.log(dms(400));  // 40°00'00"
+console.log(dms(-40));  // 320°00'00"
+console.log(dms(-420)); // 300°00'00"
