711bd10acc4e8bf30886729045fbdcbde8caa872
hungryjireh <hungryjireh@gmail.com>
2023-09-23 14:18:52 +0800

    JS101: Small Problems: Easy 5 Question 1

---
+++ JS101/small_problems/easy_5/1_cute_angles.js
@@ -0,0 +1,57 @@
+/*
+— problem: take number of degrees and return a string representing that
+  angle in degrees, minutes and seconds
+— examples: 30 -> 30°00'00", 76.73 -> 76°43'48"
+— input types: number
+— expected output types: string
+- rules: (1) 60 minutes in a degree
+  (2) 60 seconds in a minute.
+— algo:
+  - convert to string -> get integer and decimal portion
+    - integer (degrees)
+    - integer * 60 (minutes)
+  - convert to string again to get integer and decimal portion again
+    - integer * 60 (seconds)
+*/
+
+function dms(inputNumber) {
+  const inputNumberInteger = getInteger(inputNumber);
+  const inputNumberDecimal = getDecimal(inputNumber);
+  if (inputNumberDecimal !== 0) {
+    const minute = Number(inputNumberDecimal) * 60;
+    const minuteInteger = getInteger(minute);
+    const minuteIntegerString = padZeroes(minuteInteger);
+    const minuteDecimal = getDecimal(minute);
+    if (minuteDecimal !== 0) {
+      const degree = Math.round(Number(minuteDecimal) * 60);
+      const degreeString = padZeroes(degree);
+      return `${inputNumberInteger}°${minuteIntegerString}'${degreeString}"`;
+    }
+  }
+  return `${inputNumberInteger}°00'00"`;
+}
+
+function getInteger(floatingNumber) {
+  const inputFloatingNumberArray = floatingNumber.toString().split(".");
+  return inputFloatingNumberArray[0];
+}
+
+function getDecimal(floatingNumber) {
+  const inputIntegerStringArray = floatingNumber.toString().split(".");
+  if (inputIntegerStringArray.length > 1) {
+    const decimalPlace = 1 / Math.pow(10, inputIntegerStringArray[1].length);
+    return inputIntegerStringArray[1] * decimalPlace;
+  }
+  return 0;
+}
+
+function padZeroes(integer) {
+  return (integer.toString().length === 2) ? `${integer}` : `0${integer}`;
+}
+
+console.log(dms(30));           // 30°00'00"
+console.log(dms(76.73));        // 76°43'48"
+console.log(dms(254.6));        // 254°35'59"
+console.log(dms(93.034773));    // 93°02'05"
+console.log(dms(0));            // 0°00'00"
+console.log(dms(360));          // 360°00'00" or 0°00'00"
